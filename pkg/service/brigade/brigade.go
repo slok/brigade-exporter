27c8888e2a97c2be39d21123fb520f88cef53a2d
Xabier Larrakoetxea <slok69@gmail.com>
2018-08-21 09:50:23 +0200

    Add brigade storage service
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- pkg/service/brigade/brigade.go
+++ pkg/service/brigade/brigade.go
@@ -1,5 +1,11 @@
 package brigade
 
+import (
+	"github.com/Azure/brigade/pkg/storage"
+
+	"github.com/slok/brigade-exporter/pkg/log"
+)
+
 // Interface is the interface that knows how to get data from brigade
 // so the collectors can get the data.
 type Interface interface {
@@ -7,13 +13,34 @@ type Interface interface {
 }
 
 type brigade struct {
+	client storage.Store
+	logger log.Logger
 }
 
 // New returns a new brigade.Interface implementation.
-func New() Interface {
-	return &brigade{}
+func New(client storage.Store, logger log.Logger) Interface {
+	return &brigade{
+		client: client,
+		logger: logger,
+	}
 }
 
 func (b *brigade) GetProjects() ([]*Project, error) {
-	return []*Project{}, nil
+	bprs, err := b.client.GetProjects()
+	if err != nil {
+		return []*Project{}, err
+	}
+
+	prs := make([]*Project, len(bprs))
+	for i, pr := range bprs {
+		prs[i] = &Project{
+			ID:         pr.ID,
+			Name:       pr.Name,
+			Repository: pr.Repo.Name,
+			Namespace:  pr.Kubernetes.Namespace,
+			Worker:     pr.Worker.Image(),
+		}
+	}
+
+	return prs, nil
 }
