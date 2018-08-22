e828962604c3541cc9d01e0a0cf1ce5daf71adb7
Xabier Larrakoetxea <slok69@gmail.com>
2018-08-22 10:46:00 +0200

    Add brigade serve get builds
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- pkg/service/brigade/brigade.go
+++ pkg/service/brigade/brigade.go
@@ -1,6 +1,9 @@
 package brigade
 
 import (
+	"time"
+
+	azurebrigade "github.com/Azure/brigade/pkg/brigade"
 	"github.com/Azure/brigade/pkg/storage"
 
 	"github.com/slok/brigade-exporter/pkg/log"
@@ -10,6 +13,8 @@ import (
 // so the collectors can get the data.
 type Interface interface {
 	GetProjects() ([]*Project, error)
+	GetBuilds() ([]*Build, error)
+	GetJobs() ([]*Job, error)
 }
 
 type brigade struct {
@@ -33,14 +38,75 @@ func (b *brigade) GetProjects() ([]*Project, error) {
 
 	prs := make([]*Project, len(bprs))
 	for i, pr := range bprs {
+
+		// Only set image if there is an image name.
+		image := ""
+		if pr.Worker.Name != "" {
+			image = pr.Worker.Image()
+		}
+
 		prs[i] = &Project{
 			ID:         pr.ID,
 			Name:       pr.Name,
 			Repository: pr.Repo.Name,
 			Namespace:  pr.Kubernetes.Namespace,
-			Worker:     pr.Worker.Image(),
+			Worker:     image,
 		}
 	}
 
 	return prs, nil
+}
+
+func (b *brigade) GetBuilds() ([]*Build, error) {
+	bblds, err := b.client.GetBuilds()
+	if err != nil {
+		return []*Build{}, err
+	}
+
+	blds := make([]*Build, len(bblds))
+	for i, bld := range bblds {
+		blds[i] = &Build{
+			ID:        bld.ID,
+			ProjectID: bld.ProjectID,
+			Type:      bld.Type,
+			Provider:  bld.Provider,
+			Version:   bld.Revision.Commit,
+			Status:    b.getBuildStatus(bld),
+			Duration:  b.getBuildDuration(bld),
+		}
+	}
+
+	return blds, nil
+}
+
+func (b *brigade) getBuildStatus(bld *azurebrigade.Build) string {
+	if bld.Worker == nil {
+		return azurebrigade.JobUnknown.String()
+	}
+
+	return bld.Worker.Status.String()
+}
+
+func (b *brigade) getBuildDuration(bld *azurebrigade.Build) time.Duration {
+	if bld.Worker == nil {
+		return 0
+	}
+
+	var duration time.Duration
+
+	// Only get duration if build finished.
+	if bld.Worker.Status == azurebrigade.JobSucceeded || bld.Worker.Status == azurebrigade.JobFailed {
+		duration = bld.Worker.EndTime.Sub(bld.Worker.StartTime)
+	}
+
+	// Only return if is a valid duration.
+	if duration > 0 {
+		return duration
+	}
+
+	return 0
+}
+
+func (b *brigade) GetJobs() ([]*Job, error) {
+	return []*Job{}, nil
 }
--- pkg/service/brigade/model.go
+++ pkg/service/brigade/model.go
@@ -1,5 +1,7 @@
 package brigade
 
+import "time"
+
 // Project is a representation of a brigade Project required by
 // the application.
 type Project struct {
@@ -8,4 +10,25 @@ type Project struct {
 	Repository string
 	Namespace  string
 	Worker     string
+}
+
+// Build is a representation of a brigade build required by the application.
+type Build struct {
+	ID        string
+	ProjectID string
+	Type      string
+	Provider  string
+	Version   string
+	Status    string
+	Duration  time.Duration
+}
+
+// Job is a representation of a brigade build job required by the application.
+type Job struct {
+	ID       string
+	BuildID  string
+	Name     string
+	Image    string
+	Status   string
+	Duration time.Duration
 }
